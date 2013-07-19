ef30668f114a5e239ceed2917f56aef3cce470f7
Diego Martinez <PC05@Oscars-Mac.local>
2013-07-19 16:30:05 -0400

    Initial Commit

---
+++ Inventario/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Inventario
+//
+//  Created by Diego Martinez on 7/19/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Inventario/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Inventario
+//
+//  Created by Diego Martinez on 7/19/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
